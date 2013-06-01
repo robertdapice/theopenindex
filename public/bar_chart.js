e50c31acda4e906f0f85429632aac278fb9a5d00
Robert d'Apice <robertdapice@gmail.com>
2013-06-02 00:46:11 +1000

    Added labels

--- public/bar_chart.js
+++ public/bar_chart.js
@@ -1,6 +1,8 @@
 var margin = {top: 10, right: 20, bottom: 20, left: 60},
     width = 960 - margin.left - margin.right,
-    height = 1400 - margin.top - margin.bottom;
+    height = 750 - margin.top - margin.bottom;
+
+var status = "stacked";
 
 var x0 = d3.scale.ordinal()
     .rangeRoundBands([0, width], .2);
@@ -8,7 +10,7 @@ var x0 = d3.scale.ordinal()
 var x1 = d3.scale.linear();
 
 var y = d3.scale.ordinal()
-    .rangeRoundBands([height, 0], .3, 0);
+    .rangeRoundBands([height, 0], .2, 0);
 
 var yAxis = d3.svg.axis()
     .scale(y)
@@ -44,15 +46,19 @@ d3.csv("data.csv", function(error, data) {
   x0.domain(dataByGroup.map(function(d) { return d.key; }));
   x1.domain([d3.max(data, function(d) { return d.value; }), 0]).range([x0.rangeBand(), 0]);
 
-  // $.each(dataByGroup[0].values, function(d){
-  //
-  // });
+  dataByGroup[3].values.forEach(function(d){
+    d3.select("#chart_container").append("div")
+    .attr("class", "index_label")
+    .text(d.value + d.valueOffset)
+    .style("top", (11 + y(d.department)).toString() + "px")
+    .style("left", (110 + (x1(d.valueOffset + d.value))).toString() + "px");
+  });
 
   var group = svg.selectAll(".group")
       .data(dataByGroup)
     .enter().append("g")
       .attr("class", "group")
-      .attr("transform", function(d) { return "translate(" + x0(d.key) + ", 0)"; });
+      .attr("transform", function(d) { return "translate(" + x0(x0.domain()[0]) + ", 0)"; });
 
   // //Adds y-axis labels
   // group.append("text")
@@ -70,7 +76,8 @@ d3.csv("data.csv", function(error, data) {
       .attr("y", function(d) { return y(d.department); })
       .style("opacity", 0.7)
       .style("stroke", "#fff")
-      .attr("x", function(d) { return 0; })
+      .attr("class", "data_block")
+      .attr("x", function(d) { return x1(d.valueOffset); })
       .attr("height", y.rangeBand())
       .attr("width", function(d) { return x1(d.value); })
       .on("mouseover", function(d) {
@@ -81,6 +88,9 @@ d3.csv("data.csv", function(error, data) {
             return 0.6;
           }
         });
+      })
+      .on("click", function(d) {
+        toggle(d.category);
       });
 
   //Adds y-axis labels
@@ -88,31 +98,31 @@ d3.csv("data.csv", function(error, data) {
       .attr("class", "y axis")
       .call(yAxis);
 
-  //Adds change event to inputs
-  d3.selectAll("input").on("change", change);
-
-  // var timeout = setTimeout(function() {
-  //   d3.select("input[value=\"stacked\"]").property("checked", true).each(change);
-  // }, 2000);
-
-  function change() {
-    // clearTimeout(timeout);
-    if (this.value === "multiples") transitionMultiples();
-    else transitionStacked();
+  function toggle() {
+    if (status == "stacked"){
+      transitionMultiples();
+      status = "multiples";
+    } else {
+      transitionStacked();
+      status = "stacked";
+    }
   }
 
   function transitionMultiples() {
-    var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", function(d) { return "translate(" + x0(d.key) + ", 0)"; });
-    g.selectAll("rect").attr("x", function(d) { return 0; });
-    g.select(".group-label").attr("x", function(d) { return x1(d.values[0].value / 2); })
+    var t = d3.transition().duration(750),
+        g = t.selectAll(".group").attr("transform", function(d) { return "translate(" + x0(d.key) + ", 0)"; }),
+        v = d3.selectAll(".index_label").transition().duration(750).style("left", "800px");
+        g.selectAll("rect").attr("x", function(d) { return 0; });
+    // $(".index_label").animate({"left": "800px"}, 750);
+    // g.select(".group-label").attr("x", function(d) { return x1(d.values[0].value / 2); })
   }
 
   function transitionStacked() {
-    var t = svg.transition().duration(750),
+    var t = d3.transition().duration(750),
         g = t.selectAll(".group").attr("transform", "translate(" + x0(x0.domain()[0]) + ", 0)");
-    g.selectAll("rect").attr("x", function(d) { return x1(d.valueOffset); });
-    g.select(".group-label").attr("x", function(d) { return x1(d.values[0].value / 2 + d.values[0].valueOffset); })
+
+        g.selectAll("rect").attr("x", function(d) { return x1(d.valueOffset); });
+    // g.select(".group-label").attr("x", function(d) { return x1(d.values[0].value / 2 + d.values[0].valueOffset); })
   }
 
 });
