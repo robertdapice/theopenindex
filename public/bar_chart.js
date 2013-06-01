33e35005348dfe11a10ea8790745d79c4c150a8d
Robert d'Apice <robertdapice@gmail.com>
2013-06-01 22:59:26 +1000

    completed horizontal stacking

--- public/bar_chart.js
+++ public/bar_chart.js
@@ -1,31 +1,31 @@
 var margin = {top: 10, right: 20, bottom: 20, left: 60},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 1400 - margin.top - margin.bottom;
 
-var y0 = d3.scale.ordinal()
-    .rangeRoundBands([height, 0], .2);
+var x0 = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .2);
 
-var y1 = d3.scale.linear();
+var x1 = d3.scale.linear();
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1, 0);
+var y = d3.scale.ordinal()
+    .rangeRoundBands([height, 0], .3, 0);
 
-var xAxis = d3.svg.axis()
-    .scale(x);
-//    .orient("bottom");
+var yAxis = d3.svg.axis()
+    .scale(y)
+   .orient("left");
 
 var nest = d3.nest()
     .key(function(d) { return d.category; });
 
 var stack = d3.layout.stack()
     .values(function(d) { return d.values; })
-    .x(function(d) { return d.department; })
     .y(function(d) { return d.value; })
-    .out(function(d, y0) { d.valueOffset = y0; });
+    .x(function(d) { return d.department; })
+    .out(function(d, x0) { d.valueOffset = x0; });
 
 var color = d3.scale.ordinal().range(["#3182bd", "#e6550d", "#31a354", "#756bb1"]);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#bar_chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -40,67 +40,79 @@ d3.csv("data.csv", function(error, data) {
 
   var dataByGroup = nest.entries(data);
   stack(dataByGroup);
-  console.log(dataByGroup);
-  x.domain(dataByGroup[0].values.map(function(d) { return d.department; }));
-  y0.domain(dataByGroup.map(function(d) { return d.key; }));
-  y1.domain([0, d3.max(data, function(d) { return d.value; })]).range([y0.rangeBand(), 0]);
+  y.domain(dataByGroup[0].values.map(function(d) { return d.department; }));
+  x0.domain(dataByGroup.map(function(d) { return d.key; }));
+  x1.domain([d3.max(data, function(d) { return d.value; }), 0]).range([x0.rangeBand(), 0]);
+
+  // $.each(dataByGroup[0].values, function(d){
+  //
+  // });
 
   var group = svg.selectAll(".group")
       .data(dataByGroup)
     .enter().append("g")
       .attr("class", "group")
-      .attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
+      .attr("transform", function(d) { return "translate(" + x0(d.key) + ", 0)"; });
 
-  //Adds y-axis labels
-  group.append("text")
-      .attr("class", "group-label")
-      .attr("x", -6)
-      .attr("y", function(d) { return y1(d.values[0].value / 2); })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.key; });
+  // //Adds y-axis labels
+  // group.append("text")
+  //     .attr("class", "group-label")
+  //     .attr("x", -6)
+  //     .attr("y", function(d) { return y1(d.values[0].value / 2); })
+  //     .attr("dy", ".35em")
+  //     .text(function(d) { return d.key; });
 
   //Adds all data bars
   group.selectAll("rect")
       .data(function(d) { return d.values; })
     .enter().append("rect")
       .style("fill", function(d) { return color(d.category); })
-      .attr("x", function(d) { return x(d.department); })
-      .style("opacity", 0.6)
+      .attr("y", function(d) { return y(d.department); })
+      .style("opacity", 0.7)
       .style("stroke", "#fff")
-      .attr("y", function(d) { return y1(d.value); })
-      .attr("width", x.rangeBand())
-      .attr("height", function(d) { return y0.rangeBand() - y1(d.value); });
+      .attr("x", function(d) { return 0; })
+      .attr("height", y.rangeBand())
+      .attr("width", function(d) { return x1(d.value); })
+      .on("mouseover", function(d) {
+        d3.selectAll("rect").style("opacity", function(item) {
+          if (d.category == item.category) {
+            return 0.8;
+          } else {
+            return 0.6;
+          }
+        });
+      });
 
-  //Adds x-axis labels
+  //Adds y-axis labels
   group.filter(function(d, i) { return !i; }).append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + y0.rangeBand() + ")")
-      .call(xAxis);
+      .attr("class", "y axis")
+      .call(yAxis);
 
   //Adds change event to inputs
   d3.selectAll("input").on("change", change);
 
-  var timeout = setTimeout(function() {
-    d3.select("input[value=\"stacked\"]").property("checked", true).each(change);
-  }, 2000);
+  // var timeout = setTimeout(function() {
+  //   d3.select("input[value=\"stacked\"]").property("checked", true).each(change);
+  // }, 2000);
 
   function change() {
-    clearTimeout(timeout);
+    // clearTimeout(timeout);
     if (this.value === "multiples") transitionMultiples();
     else transitionStacked();
   }
 
   function transitionMultiples() {
     var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
-    g.selectAll("rect").attr("y", function(d) { return y1(d.value); });
-    g.select(".group-label").attr("y", function(d) { return y1(d.values[0].value / 2); })
+        g = t.selectAll(".group").attr("transform", function(d) { return "translate(" + x0(d.key) + ", 0)"; });
+    g.selectAll("rect").attr("x", function(d) { return 0; });
+    g.select(".group-label").attr("x", function(d) { return x1(d.values[0].value / 2); })
   }
 
   function transitionStacked() {
     var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", "translate(0," + y0(y0.domain()[0]) + ")");
-    g.selectAll("rect").attr("y", function(d) { return y1(d.value + d.valueOffset); });
-    g.select(".group-label").attr("y", function(d) { return y1(d.values[0].value / 2 + d.values[0].valueOffset); })
+        g = t.selectAll(".group").attr("transform", "translate(" + x0(x0.domain()[0]) + ", 0)");
+    g.selectAll("rect").attr("x", function(d) { return x1(d.valueOffset); });
+    g.select(".group-label").attr("x", function(d) { return x1(d.values[0].value / 2 + d.values[0].valueOffset); })
   }
+
 });
