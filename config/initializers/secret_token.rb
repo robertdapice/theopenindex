1d10143c03e929b70c0132ebde696bd5f2dd19e6
Robert d'Apice <robertdapice@gmail.com>
2013-06-01 18:04:21 +1000

    setup theopenindex

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theopenindex::Application.config.secret_token = 'e3d82a033597734de00a4cefd110d25ffaa20ea2627b0dafe85cb3b42184949c74d4a102e0fd78a935dfe673ba7e45922f5f9c3c0f6f0b003fd3731bab0bb29f'
